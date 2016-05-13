4860a181d2a741c5ce8e4faee401de6477560927
Sandeep Kumar <pksandeepkumar@gmail.com>
2016-05-13 17:45:06 +0530

    Modified font example

--- FontTest/app/src/main/java/texus/fonttest/MainActivity.java
+++ FontTest/app/src/main/java/texus/fonttest/MainActivity.java
@@ -3,8 +3,10 @@ package texus.fonttest;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv ;
@@ -25,11 +27,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setFont3( View view) {
-        setFont("JLSSpaceGothicR_NC.otf");
+        setFont("Museo700-Regular.ttf");
     }
 
     public void setFont( String fontname) {
-        Typeface face= Typeface.createFromAsset(getAssets(), "font/" + fontname);
-        tv.setTypeface(face);
+        try {
+            Log.e("MainActivity","Font:" + fontname);
+            Typeface face= Typeface.createFromAsset(getAssets(), "fonts/" + fontname);
+            tv.setTypeface(face);
+        } catch ( Exception e) {
+            e.printStackTrace();;
+            Toast.makeText(this,"Font:" + fontname + " cannot be set", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
