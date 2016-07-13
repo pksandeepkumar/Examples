85846e357b5b6a2c7def2d76c2e78c914221b7fb
Sandeep Kumar <pksandeepkumar@gmail.com>
2016-07-13 22:42:33 +0530

    Stagged Grid Recyclerview Auto fit example added

---
+++ StaggedGridRecyclerView/app/src/main/java/sbl/staggedgridrecyclerview/MainActivity.java
@@ -0,0 +1,118 @@
+package sbl.staggedgridrecyclerview;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private StaggeredGridLayoutManager gaggeredGridLayoutManager;
+    private Context context =null;
+    RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        context = this;
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+
+//        gaggeredGridLayoutManager = new StaggeredGridLayoutManager(2, 1);
+
+//        mRecyclerView.getViewTreeObserver().addOnGlobalLayoutListener(
+//                new ViewTreeObserver.OnGlobalLayoutListener() {
+//                    @Override
+//                    public void onGlobalLayout() {
+//                        mRecyclerView.getViewTreeObserver().removeOnGlobalLayoutListener(this);;
+//                        int viewWidth = mRecyclerView.getMeasuredWidth();
+//                        float cardViewWidth = context.getResources().getDimension(R.dimen.card_width);
+//                        int newSpanCount = (int) Math.floor(viewWidth / cardViewWidth);
+//                        gaggeredGridLayoutManager.setSpanCount(newSpanCount);
+//                        gaggeredGridLayoutManager.requestLayout();
+//                    }
+//                });
+
+
+//        mRecyclerView.setHasFixedSize(true);
+
+
+//        mRecyclerView.setLayoutManager(gaggeredGridLayoutManager);
+
+        List<ItemObjects> gaggeredList = getListItemData();
+
+        SolventRecyclerViewAdapter rcAdapter = new SolventRecyclerViewAdapter(MainActivity.this, gaggeredList);
+        mRecyclerView.setAdapter(rcAdapter);
+    }
+
+
+
+
+    private List<ItemObjects> getListItemData(){
+        List<ItemObjects> listViewItems = new ArrayList<ItemObjects>();
+        listViewItems.add(new ItemObjects("Alkane", R.drawable.one));
+        listViewItems.add(new ItemObjects("Ethane", R.drawable.two));
+        listViewItems.add(new ItemObjects("Alkyne", R.drawable.three));
+        listViewItems.add(new ItemObjects("Benzene", R.drawable.four));
+        listViewItems.add(new ItemObjects("Amide", R.drawable.one));
+        listViewItems.add(new ItemObjects("Amino Acid", R.drawable.two));
+        listViewItems.add(new ItemObjects("Phenol", R.drawable.three));
+        listViewItems.add(new ItemObjects("Carbonxylic", R.drawable.four));
+        listViewItems.add(new ItemObjects("Nitril", R.drawable.one));
+        listViewItems.add(new ItemObjects("Ether", R.drawable.two));
+        listViewItems.add(new ItemObjects("Ester", R.drawable.three));
+        listViewItems.add(new ItemObjects("Alcohol", R.drawable.four));
+        listViewItems.add(new ItemObjects("Ethane", R.drawable.two));
+        listViewItems.add(new ItemObjects("Alkyne", R.drawable.three));
+        listViewItems.add(new ItemObjects("Benzene", R.drawable.four));
+        listViewItems.add(new ItemObjects("Amide", R.drawable.one));
+        listViewItems.add(new ItemObjects("Amino Acid", R.drawable.two));
+        listViewItems.add(new ItemObjects("Phenol", R.drawable.three));
+        listViewItems.add(new ItemObjects("Carbonxylic", R.drawable.four));
+        listViewItems.add(new ItemObjects("Nitril", R.drawable.one));
+        listViewItems.add(new ItemObjects("Ether", R.drawable.two));
+        listViewItems.add(new ItemObjects("Ester", R.drawable.three));
+        listViewItems.add(new ItemObjects("Alcohol", R.drawable.four));
+        listViewItems.add(new ItemObjects("Ethane", R.drawable.two));
+        listViewItems.add(new ItemObjects("Alkyne", R.drawable.three));
+        listViewItems.add(new ItemObjects("Benzene", R.drawable.four));
+        listViewItems.add(new ItemObjects("Amide", R.drawable.one));
+        listViewItems.add(new ItemObjects("Amino Acid", R.drawable.two));
+        listViewItems.add(new ItemObjects("Phenol", R.drawable.three));
+        listViewItems.add(new ItemObjects("Carbonxylic", R.drawable.four));
+        listViewItems.add(new ItemObjects("Nitril", R.drawable.one));
+        listViewItems.add(new ItemObjects("Ether", R.drawable.two));
+        listViewItems.add(new ItemObjects("Ester", R.drawable.three));
+        listViewItems.add(new ItemObjects("Alcohol", R.drawable.four));
+        listViewItems.add(new ItemObjects("Ethane", R.drawable.two));
+        listViewItems.add(new ItemObjects("Alkyne", R.drawable.three));
+        listViewItems.add(new ItemObjects("Benzene", R.drawable.four));
+        listViewItems.add(new ItemObjects("Amide", R.drawable.one));
+        listViewItems.add(new ItemObjects("Amino Acid", R.drawable.two));
+        listViewItems.add(new ItemObjects("Phenol", R.drawable.three));
+        listViewItems.add(new ItemObjects("Carbonxylic", R.drawable.four));
+        listViewItems.add(new ItemObjects("Nitril", R.drawable.one));
+        listViewItems.add(new ItemObjects("Ether", R.drawable.two));
+        listViewItems.add(new ItemObjects("Ester", R.drawable.three));
+        listViewItems.add(new ItemObjects("Alcohol", R.drawable.four));
+        listViewItems.add(new ItemObjects("Ethane", R.drawable.two));
+        listViewItems.add(new ItemObjects("Alkyne", R.drawable.three));
+        listViewItems.add(new ItemObjects("Benzene", R.drawable.four));
+        listViewItems.add(new ItemObjects("Amide", R.drawable.one));
+        listViewItems.add(new ItemObjects("Amino Acid", R.drawable.two));
+        listViewItems.add(new ItemObjects("Phenol", R.drawable.three));
+        listViewItems.add(new ItemObjects("Carbonxylic", R.drawable.four));
+        listViewItems.add(new ItemObjects("Nitril", R.drawable.one));
+        listViewItems.add(new ItemObjects("Ether", R.drawable.two));
+        listViewItems.add(new ItemObjects("Ester", R.drawable.three));
+        listViewItems.add(new ItemObjects("Alcohol", R.drawable.four));
+
+        return listViewItems;
+    }
+}
